14b97149604fc502fa0daf88e51cfcf10d6b89b4
jim <jchoncholas@gmail.com>
2020-01-23 16:03:26 -0500

    squash warm/cold plots

--- test/GeoResolver_2020-01-23_localrop_results/plotter.py
+++ test/GeoResolver_2020-01-23_localrop_results/plotter.py
@@ -6,43 +6,52 @@ import matplotlib.backends.backend_pdf
 
 
 def main():
-    allexps = ['direct_resolve', '20km_resolve', '630km_resolve']
+    allexps = ['direct_resolve']#, '20km_resolve', '630km_resolve']
+    wc = ["warm", "cold"]
 
-    for experiment in allexps:
+    for generic_experiment in allexps:
+        wc_experiments = [generic_experiment+"_warm", generic_experiment+"_cold"];
 
-        filename = experiment + '.txt'
+        for experiment in wc_experiments:
+            filename = experiment + '.csv'
 
-        with open(filename) as f:
+            with open(filename) as f:
+                df = pd.read_csv(filename)#, header=None)
+                #df.info()
 
-            df = pd.read_csv(filename)#, header=None)
-            #df.info()
-            #print ""
+                fig_id = 0
+                df = df.sort_values(by='query_time')
 
-            fig_id = 0
-            df = df.sort_values(by='Query Time')
+                # sorted data
+                plt.figure(fig_id)
+                plt.plot(df['query_time'].values.astype(float)*1000, label=experiment)
 
-            # sorted data
-            plt.figure(fig_id)
-            plt.plot(df['Query Time'].values.astype(float)*1000)
-            plt.xlabel('Query Number')
-            plt.ylabel('DNS Query Time (ms)')
-            plt.title("All (Sorted) Query Time for Experiment: " + experiment)
-            #plt.show()
-            pdf = matplotlib.backends.backend_pdf.PdfPages(experiment + "_sorted.pdf")
-            pdf.savefig()
-            pdf.close()
+                # save pdf of all last experiments
+                if (experiment == wc_experiments[-1]):
+                    plt.xlabel('Query Number')
+                    plt.ylabel('DNS Query Time (ms)')
+                    plt.title("All (Sorted) Query Times")
+                    plt.legend()
+                    #plt.show()
+                    pdf = matplotlib.backends.backend_pdf.PdfPages(generic_experiment + "_sorted.pdf")
+                    pdf.savefig()
+                    pdf.close()
 
-            # CDF
-            fig_id += 1
-            plt.figure(fig_id)
-            plt.plot(df['Query Time'].values.astype(float)*1000, np.linspace(0, 1, len(df['Query Time']), endpoint=False))
-            plt.xlabel('DNS Query Time (ms)')
-            plt.ylabel('% of Queries Resolved')
-            plt.title("CDF of Query Time for Experiment: " + experiment)
-            #plt.show()
-            pdf = matplotlib.backends.backend_pdf.PdfPages(experiment + "_cdf.pdf")
-            pdf.savefig()
-            pdf.close()
+                # CDF
+                fig_id += 1
+                plt.figure(fig_id)
+                plt.plot(df['query_time'].values.astype(float)*1000, np.linspace(0, 1, len(df['query_time']), endpoint=False), label=experiment)
+
+                # save pdf of all last experiments
+                if (experiment == wc_experiments[-1]):
+                    plt.xlabel('DNS Query Time (ms)')
+                    plt.ylabel('% of Queries Resolved')
+                    plt.title("CDF of Query Time")
+                    plt.legend()
+                    #plt.show()
+                    pdf = matplotlib.backends.backend_pdf.PdfPages(generic_experiment + "_cdf.pdf")
+                    pdf.savefig()
+                    pdf.close()
 
 if __name__ == "__main__":
   main()
